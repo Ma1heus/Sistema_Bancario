18af306b1311613d2be07333efb4c68e0fdcc08f
Matheus Luz Rodrigues <86856853+Ma1heus@users.noreply.github.com>
2025-04-02 21:03:31 -0300

    sistema_usuario
    Terminei o sistema de criação do usuário. E também fiz algumas pequenas correções.

--- main.py
+++ main.py
@@ -17,10 +17,13 @@ def menu () :
     [d] Depositar
     [s] Sacar
     [e] Extrato
+    [nc] Nova conta
+    [lc] Listar contas
+    [nu] Novo usuário
     [l] Sair\033[m
 
     => """
-    return input (menu).lower()[0]
+    return input (menu).lower()
 
 
 def depositos_realizados(saldo, extrato):
@@ -72,43 +75,107 @@ def exibir_extrato(saldo, /, *, extrato):
     sleep(1)
 
 
-saldo = 0
-vezes_utilizadas = 0
-extrato = ''
-numero_saques = 0
+def criar_usuario(usuarios):
+    linhas()
+    cpf = input('Informe o CPF (somente número): ')
+    usuario = filtrar_usuario(cpf, usuarios)
 
-linhas()
-print(f'\033[36m{'Bem-Vindo ao Banco do Brasil':^40}\033[m')
-linhas()
-print('Escolha uma opção para começarmos: ')
+    if usuario:
+        print('\n\033[31mJá existe usuário com esse CPF!\033[m')
 
-while True:
-    if vezes_utilizadas == 0:
-        opcao = menu()
-        vezes_utilizadas = 1
+    nome = input('Informe o nome completo: ')
+    data_nascimento = input('Informe a data de nascimento (dd-mm-aaaa): ')
+    endereco = input('Informe o endereço (logradouro, nro/bairro - cidade/sigla estado): ')
 
-    else:
-        linhas ()
-        print('Escolha sua próxima operação: ')
-        opcao = menu()
+    usuarios.append({"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "endereco": endereco})
 
-    if opcao == 'd':
-        saldo, extrato = depositos_realizados(saldo, extrato)
+    print('\033[33mUsuário criado com sucesso!\033[m')
 
-    elif opcao == 's':
-        saldo, extrato = saques(saldo=saldo, extrato=extrato, numero_saques=numero_saques)
 
-    elif opcao == 'e':
-        exibir_extrato(saldo, extrato=extrato)
+def filtrar_usuario(cpf, usuarios):
+    usuarios_filtrados = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
+    return usuarios_filtrados[0] if usuarios_filtrados else None
 
-    elif opcao == 'l':
-        linhas ()
-        print('Obrigado pela sua preferência. Ótimo dia!')
-        linhas ()
-        sleep (1)
-        break
 
-    else:
+def criar_conta(agencia, numero_conta, usuarios):
+    linhas()
+    cpf = input('Digite seu CPF: ')
+    usuario = filtrar_usuario(cpf, usuarios)
+
+    if usuario:
+        print("\n\033[32mConta criada com sucesso!\033[m")
+        return {"agencia": agencia, "numero_conta": numero_conta, "usuario": usuario}
+
+    print("\n\033[31mUsuário não encontrado, fluxo de criação de conta encerrado! \033[m")
+
+
+def listar_contas(contas):
+    for conta in contas:
+        linha = f"""\
+            Agência:\t{conta['agencia']}
+            C/C:\t\t{conta['numero_conta']}
+            Titular:\t{conta['usuario']['nome']}
+        """
         linhas()
-        print('\033[31mOperação inválida, por favor selecione novamente a operação desejada.\033[m')
-        sleep (1)
+        print(linha)
+
+
+def main():
+    AGENCIA = "0001"
+    saldo = 0
+    vezes_utilizadas = 0
+    extrato = ''
+    usuarios = []
+    contas = []
+    numero_saques = 0
+
+    linhas()
+    print(f'\033[36m{'Bem-Vindo ao Banco do Brasil':^40}\033[m')
+    linhas()
+    print('Escolha uma opção para começarmos: ')
+
+    while True:
+        if vezes_utilizadas == 0:
+            opcao = menu()
+            vezes_utilizadas = 1
+
+        else:
+            linhas ()
+            print('Escolha sua próxima operação: ')
+            opcao = menu()
+
+        if opcao == 'd':
+            saldo, extrato = depositos_realizados(saldo, extrato)
+
+        elif opcao == 's':
+            saldo, extrato = saques(saldo=saldo, extrato=extrato, numero_saques=numero_saques)
+
+        elif opcao == 'e':
+            exibir_extrato(saldo, extrato=extrato)
+
+        elif opcao == 'nu':
+            criar_usuario (usuarios)
+
+        elif opcao == 'nc':
+            numero_conta = len (contas) + 1
+            conta = criar_conta (AGENCIA, numero_conta, usuarios)
+
+            if conta :
+                contas.append (conta)
+
+        elif opcao == 'lc' :
+            listar_contas (contas)
+
+        elif opcao == 'l':
+            linhas ()
+            print('Obrigado pela sua preferência. Ótimo dia!')
+            linhas ()
+            sleep (1)
+            break
+
+        else:
+            linhas()
+            print('\033[31mOperação inválida, por favor selecione novamente a operação desejada.\033[m')
+            sleep (1)
+
+main()
